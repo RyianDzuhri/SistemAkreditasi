1d61fd79cacde61f6b3770d836e6f916a255dbfb
RyianDzuhri <muhammadryiandzuhri@gmail.com>
2024-12-30 17:34:57 +0800

    Add new

--- controllers/PenilaianProdiController.php
+++ controllers/PenilaianProdiController.php
@@ -71,6 +71,10 @@ class PenilaianProdiController extends Controller
     {
         $model = new PenilaianProdi();
 
+        // Ambil data untuk dropdown
+        $listProdi = \app\models\Prodi::find()->select(['nama_prodi'])->indexBy('id_prodi')->column();
+        $listIndikator = \app\models\Indikator::find()->select(['nama_indikator'])->indexBy('id_indikator')->column();
+
         if ($this->request->isPost) {
             if ($model->load($this->request->post()) && $model->save()) {
                 return $this->redirect(['view', 'id_penilaian' => $model->id_penilaian, 'id_prodi' => $model->id_prodi, 'id_indikator' => $model->id_indikator]);
@@ -81,21 +85,18 @@ class PenilaianProdiController extends Controller
 
         return $this->render('create', [
             'model' => $model,
+            'listProdi' => $listProdi,
+            'listIndikator' => $listIndikator,
         ]);
     }
 
-    /**
-     * Updates an existing PenilaianProdi model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param int $id_penilaian Id Penilaian
-     * @param int $id_prodi Id Prodi
-     * @param int $id_indikator Id Indikator
-     * @return string|\yii\web\Response
-     * @throws NotFoundHttpException if the model cannot be found
-     */
     public function actionUpdate($id_penilaian, $id_prodi, $id_indikator)
     {
         $model = $this->findModel($id_penilaian, $id_prodi, $id_indikator);
+
+        // Ambil data untuk dropdown
+        $listProdi = \app\models\Prodi::find()->select(['nama_prodi'])->indexBy('id_prodi')->column();
+        $listIndikator = \app\models\Indikator::find()->select(['nama_indikator'])->indexBy('id_indikator')->column();
 
         if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id_penilaian' => $model->id_penilaian, 'id_prodi' => $model->id_prodi, 'id_indikator' => $model->id_indikator]);
@@ -103,18 +104,11 @@ class PenilaianProdiController extends Controller
 
         return $this->render('update', [
             'model' => $model,
+            'listProdi' => $listProdi,
+            'listIndikator' => $listIndikator,
         ]);
     }
 
-    /**
-     * Deletes an existing PenilaianProdi model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param int $id_penilaian Id Penilaian
-     * @param int $id_prodi Id Prodi
-     * @param int $id_indikator Id Indikator
-     * @return \yii\web\Response
-     * @throws NotFoundHttpException if the model cannot be found
-     */
     public function actionDelete($id_penilaian, $id_prodi, $id_indikator)
     {
         $this->findModel($id_penilaian, $id_prodi, $id_indikator)->delete();
